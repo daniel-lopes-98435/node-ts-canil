24dbc05d8d392943443fcd705c231395ce3f175e
daniel-lopes-98435 <daniel_slopes@hotmail.com>
2021-09-03 10:38:09 -0300

    Controller settings

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,14 @@
 import { Router } from 'express';
+import * as pageController from '../controllers/pageController';
+import * as searchController from '../controllers/searchController';
+
 const router = Router();
-router.get('/', (req,res)=>{
-    res.send('home');
-})
+
+router.get('/', pageController.home);
+router.get('/dogs', pageController.dogs);
+router.get('/dogs', pageController.cats);
+router.get('/dogs', pageController.fishes)
+
+router.get('/search', searchController.search);
+
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
-import mainRoute from './routes/index'
+import mainRoutes from './routes/index'
 
 
 dotenv.config();
@@ -14,7 +14,7 @@ server.set('views', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
-server.use(mainRoute);
+server.use(mainRoutes);
 server.use((req, res)=>{
     res.status(404).send('Página não encontrada');
 })
